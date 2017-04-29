6a6e2e33a4993d1e7ba61dfcd54e6787d122a45a
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-04-30 01:25:01 +0800

    updated deps

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ import PackageDescription
 let package = Package(
   name: "Requests",
   dependencies: [
-    .Package(url: "https://github.com/bytethenoodle/Inquiline", majorVersion: 0, minor: 3),
+    .Package(url: "https://github.com/bytethenoodle/Inquiline", "0.3.4"),
   ]
 )
